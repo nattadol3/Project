01613013a756b73b6888c128195069b1e4f2e47b
Nattadol Tongbu <nattadol.t@kkumail.com>
2023-04-07 10:01:07 +0700

    Set to full screen

--- desktop/src/com/game/project/DesktopLauncher.java
+++ desktop/src/com/game/project/DesktopLauncher.java
@@ -8,7 +8,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
-		config.setWindowedMode(1100, 720);
+		config.setFullscreenMode(Lwjgl3ApplicationConfiguration.getDisplayMode());
 		config.setTitle("Project");
 		new Lwjgl3Application(new project(), config);
 	}
