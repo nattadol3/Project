30a22cd142bb6e2fa69f84476e618a1880b250cb
Nattadol Tongbu <nattadol.t@kkumail.com>
2023-04-07 11:04:34 +0700

    .

--- desktop/src/com/game/project/DesktopLauncher.java
+++ desktop/src/com/game/project/DesktopLauncher.java
@@ -8,8 +8,8 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
-		config.setWindowedMode(1000, 720);
-		//config.setFullscreenMode(Lwjgl3ApplicationConfiguration.getDisplayMode());
+		//config.setWindowedMode(1000, 720);
+		config.setFullscreenMode(Lwjgl3ApplicationConfiguration.getDisplayMode());
 		config.setTitle("Project");
 		new Lwjgl3Application(new project(), config);
 	}
